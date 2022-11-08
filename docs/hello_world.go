3c3a53207e970838a187eb8dca2a61a00a8ad2bc
NamLuu <koongnam@NamLuus-MacBook-Pro.local>
2022-11-08 09:18:11 +0700

    build golang

--- docs/hello_world.go
+++ docs/hello_world.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	fmt.Print("Hello world!")
 	fmt.Print("Hello world - feature!!!!")
-	fmt.Print("Hello world - setup golang!!!!")
+	fmt.Print("Hello world - setup golang!!!!!")
 
 }
